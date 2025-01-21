6825fdfdfa8259e0849db3233a4c7324ec18e3f8
Chaitanya Kumbhakarna <chaitanya.kumbhakarna@arbitersports.com>
2025-01-21 13:25:12 +0530

    adds and handle any amount of numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -12,4 +12,12 @@ RSpec.describe StringCalculator do
       expect(calculator.add(nil)).to eq(0)
     end
   end
+
+  context 'handle any amount of numbers' do
+    it 'returns sum for an given number string' do
+      expect(calculator.add("1,2,3")).to eq(6)
+      expect(calculator.add("4,5,6,7,8")).to eq(30)
+      expect(calculator.add("10,20,30,40,50,60")).to eq(210)
+    end
+  end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,5 +1,7 @@
 class StringCalculator
   def add(num_string)
     return 0 if num_string.nil? || num_string.empty? #we can write .blank? in rails for more cases like string with spaces but still empty
+
+    num_string.split(",").map(&:to_i).inject(:+)
   end
 end
